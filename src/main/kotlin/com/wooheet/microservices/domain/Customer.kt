6c7d8f8f4ecb7918b8beb57bddb8f6dfccf79441
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-11 14:16:54 +0900

    Add domain

--- src/main/kotlin/com/wooheet/microservices/domain/Customer.kt
+++ src/main/kotlin/com/wooheet/microservices/domain/Customer.kt
@@ -1,5 +1,13 @@
 package com.wooheet.microservices.domain
 
-data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone? = null) {
-  data class Telephone(var countryCode: String = "", var telephoneNumber: String = "")
+data class Customer(
+  var id: Int = 0,
+  var name: String = "",
+  var telephone:
+  Telephone? = null
+) {
+  data class Telephone(
+    var countryCode: String = "",
+    var telephoneNumber: String = ""
+  )
 }
