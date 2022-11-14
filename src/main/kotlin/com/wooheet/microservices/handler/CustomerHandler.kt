56469313c3431a419d414b7da92ba4e671afe266
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-14 15:44:05 +0900

    Add error handler to create

--- src/main/kotlin/com/wooheet/microservices/handler/CustomerHandler.kt
+++ src/main/kotlin/com/wooheet/microservices/handler/CustomerHandler.kt
@@ -4,27 +4,31 @@ import com.wooheet.microservices.domain.Customer
 import com.wooheet.microservices.service.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.BodyInserters.fromValue
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.*
 import org.springframework.web.reactive.function.server.bodyToMono
+import reactor.core.publisher.onErrorResume
 import java.net.URI
 
 @Component
 class CustomerHandler(val customerService: CustomerService) {
-    
+
     fun get(serverRequest: ServerRequest) =
         customerService.getCustomer(serverRequest.pathVariable("id").toInt())
-            .flatMap { ok().body(fromValue(it)) }
+            .flatMap { ok().body(fromObject(it)) }
             .switchIfEmpty(status(HttpStatus.NOT_FOUND).build())
-    
+
     fun search(serverRequest: ServerRequest) =
         ok().body(customerService.searchCustomers(serverRequest.queryParam("nameFilter")
             .orElse("")), Customer::class.java)
 
     fun create(serverRequest: ServerRequest) =
         customerService.createCustomer(serverRequest.bodyToMono()).flatMap {
-            created(URI.create("/functional/customer/${it.id}")).build()
+            created(URI.create("functional/customer/${it.id}")).build()
+        }.onErrorResume(Exception::class) {
+            badRequest().body(fromObject(ErrorResponse("error creating customer",
+            it.message ?: "error")))
         }
-    
+
 }
