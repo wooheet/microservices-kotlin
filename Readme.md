8d9afb9d3c89cb1ec2f37901a8057f8ecfed6f19
haewon <n2203020@N2203020ui-MacBookPro.local>
2022-11-17 18:04:11 +0900

    Add gcp lib to gradle

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Microservices
+
+## Spec
+- **Spring Boot 2.7**
+- **JDK 11**
+- **Spring Initializer**
+- **Gradle**
+- **Component Scan**
+- **Spring application context**
+- **Spring configuration**
+- **Spring Expression Language**
+- **Reactive Core**
+- **JPA**
+- **Google Cloud SQL**
+- **Kubernetes**
+- **Istio**
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudGcpVersion"] = "3.4.0"
 extra["springCloudVersion"] = "2021.0.5"
 
 dependencies {
@@ -27,7 +28,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
-    implementation("org.springframework.cloud:spring-cloud-sleuth-core")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
